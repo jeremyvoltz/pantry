4f763db02b8f433d4b9376f1c0ff39477f366bcb
jeremyvoltz <jeremyvoltz@gmail.com>
2016-01-13 01:42:59 -0500

    remove essential checkboxes on add food page

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ status_code = [(0, "buy"),
 
 
 # Create M2M table
-location_table = db.Table('location_table',
+location_table = db.Table('location_table', 
                            db.Column('food_id', db.Integer, db.ForeignKey('food.id')),
                            db.Column('store_id', db.Integer, db.ForeignKey('store.id'))
                            )
@@ -66,19 +66,20 @@ class Store(db.Model):
 def index():
     return render_template('home.html')
 
+
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     food = Food.query.order_by(Food.name)
     if request.method == 'POST':
         for f in food:
-            if request.form.get(f) == '1':
-                food = Food.query.filter_by(name=key).first()
-                food.status = 0
+            if request.form.get(f.name) == '1':
+                f.status = 0
                 db.session.commit()
         for i in range(5):
             name = request.form.get(str(i))
             if name:
-                essential = bool(request.form.get("checkbox_" + str(i)))
+                # essential = bool(request.form.get("checkbox_" + str(i)))
+                essential = False
                 food = Food(name=name, essential=essential, status=0)
                 db.session.add(food)
                 db.session.commit()
@@ -120,12 +121,13 @@ def stores():
     if request.method == 'POST':
         for f in food:
             food = Food.query.filter_by(name=f.name).first()
+            food.locations = []
+            db.session.commit()
             for store in locations:
                 if request.form.get(f.name + "_" + store.name) == '1':
                     food.locations.append(store)
-                elif store in food.locations:
-                    food.locations.remove(store)
-
+                # elif store in food.locations:
+                    # food.locations.remove(store)
                 db.session.commit()
         
         return redirect(url_for('index'))
